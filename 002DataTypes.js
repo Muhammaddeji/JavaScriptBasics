6d450ea5a22a65c9763afb4302c93be17a7f2ab2
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-06-14 15:31:34 +0100

    002DataTypes.js

---
+++ 002DataTypes.js
@@ -0,0 +1,40 @@
+
+let name = "Sheriff";
+let greeting = "Hello";
+console.log(name, typeof name);
+console.log(greeting, typeof greeting);
+
+let age = 22;
+let price = 15.75;
+console.log(age, typeof age);
+console.log(price, typeof price);
+
+let isStudent = true;
+let hasLicense = false;
+console.log(isStudent, typeof IsStudent);
+console.log(hasLicense, typeof hasLicense);
+
+let score;
+let user;
+console.log (score, typeof score);
+console.log (user, typeof user);
+
+let data = null;
+let selected = null;
+console.log (data, typeof data);
+console.log(selected, typeof selected);
+
+let id = Symbol("id");
+let token = Symbol("auth");
+console.log(id, typeof id);
+console.log(token, typeof token);
+
+let bigNumber = 1234567890123456789012345678901234567890n;
+let anotherBig = BigInt(987654321987654321987654321n);
+console.log(bigNumber, typeof bigNumber);
+console.log(anotherBig, typeof anotherBig);
+
+let person = {name: "John", age: 50};
+let numbers = [1,2,3,4,5];
+console.log(person, typeof person);
+console.log(numbers, typeof numbers);
