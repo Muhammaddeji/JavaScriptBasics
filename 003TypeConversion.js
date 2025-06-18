fa2a935365bfd414039895727a7ab630decbabe3
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-06-18 17:58:13 +0100

    003TypeConversion.js

---
+++ 003TypeConversion.js
@@ -0,0 +1,14 @@
+let num = 100;
+let strNum = String(num);
+console.log("Value:", strNum);
+console.log("Type:", typeof strNum);
+
+let str = '200';
+let numstr = Number (str);
+console.log ("Value:", numstr);
+
+let value = 1;
+let boolValue = Boolean(value)
+console.log("Value:", boolValue);
+console.log("Type:", typeof boolValue);
+
