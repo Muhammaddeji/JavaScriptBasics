4b0274b567c537efa22d205ab78a5e4fc7089955
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-06-27 18:46:34 +0100

    0011WhileLopp.js

---
+++ 0011WhileLopp.js
@@ -0,0 +1,20 @@
+// while loop
+// do while loop
+//for loop
+
+// repeat this statement 5 times 
+
+
+let i = 1;
+
+
+do
+{
+     console.log("Hi", i);
+     i++;
+}while(i<=5)
+
+// i++; is called post increament
+
+//to do that multiple times and not repeatiing oneself (DRY), We use loop.
+
