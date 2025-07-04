c7bdf434b90766a68902707267c0e7c0e5102494
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-07-04 16:27:13 +0100

    0014ComplexObjects.js

---
+++ 0014ComplexObjects.js
@@ -0,0 +1,11 @@
+let alien = {
+    name: 'Adedeji',
+    tech: '50',
+    laptop: {
+        cpu: 'i7',
+        ram: '4',
+        brand: 'Asus'
+    }
+}
+
+console.log(alien.laptop.brand.length);
