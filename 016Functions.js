15ab331c82e156cc047845e6306711278b589ade
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-07-08 20:51:34 +0100

    016Functions.js

---
+++ 016Functions.js
@@ -0,0 +1,12 @@
+// functions in javascript / funtion return and passing a value
+
+
+function greet(user)
+{
+    return "Hello user"
+}
+
+let user = 'Hello ${user}!'
+let str = greet(user);
+console.log(str);
+
